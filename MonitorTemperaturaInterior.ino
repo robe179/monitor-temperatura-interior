222db7c6e68deceba785f60b649110a4e607ec53
Robe <barj179@gmail.com>
2021-03-06 12:19:12 -0600

    agregamos grados Fahrenheit

--- MonitorTemperaturaInterior.ino
+++ MonitorTemperaturaInterior.ino
@@ -5,20 +5,21 @@
 // with the arduino pin number it is connected to
 const int rs = 13, en = 12, d4 = 11, d5 = 10, d6 = 9, d7 = 8;
 const int lm35 = 0;
-float temperatura; 
+float tempCelsius; 
+float tempFahrenheit;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 //Symbol grade
 byte grade[8] = {
-  B01100,
-  B10010,
-  B10010,
-  B01100,
+  B00110,
+  B01001,
+  B01001,
+  B00110,
   B00000,
   B00000,
   B00000,
   B00000
-}
+};
 
 void setup() {
   // Configuramos el puerto serial a 9600 bps
@@ -28,35 +29,41 @@ void setup() {
   lcd.begin(16, 2);
   lcd.clear();
 
-  lcd.setCursor(0,0);
-  lcd.print("C");
+  lcd.setCursor(5,0);
   lcd.write(byte(0));
-  lcd.print(":");
+  lcd.print("C");
 
-  lcd.setCursor(5,0)
-  lcd.print("raw:")
+  lcd.setCursor(8,0);
+  lcd.print("raw:");
+
+  lcd.setCursor(5,1);
+  lcd.write(byte(0));
+  lcd.print("F");
 }
 
 void loop() {
 
   int value = analogRead(lm35);
-  //los 5k es por el voltaje, por ahora me da un voltaje de 4.3
-  //float millivolts = (value / 1023.0) * 5000;
-  float millivolts = (value / 1023.0) * 4990;
-  temperatura = millivolts / 10; 
+  //los 5k es por el voltaje
+  float millivolts = (value / 1023.0) * 5000;
+  tempCelsius = millivolts / 10; 
   Serial.print(value);
   Serial.print(" ");
-  Serial.print(temperatura);
+  Serial.print(tempCelsius);
   Serial.println(" C");
+  tempFahrenheit = ( tempCelsius * (9/5)) + 32;
    
   // Esperamos un tiempo para repetir el loop
   delay(1000);
   // set the cursor to column 0, line 1
   // (note: line 1 is the second row, since counting begins with 0):
-  lcd.setCursor(3, 0);
-  lcd.print(temperatura);
+  lcd.setCursor(0, 0);
+  lcd.print(tempCelsius);
 
-  lcd.setCursor(14,0);
+  lcd.setCursor(0, 1);
+  lcd.print(tempFahrenheit);
+
+  lcd.setCursor(12,0);
   lcd.print(value);
   
 }
